53c962fd98967035a83aae898d99748feea6121e
Noer Paanakker <noer@hackyourfuture.net>
2020-05-20 13:41:47 +0200

    added test files, 3 versions

---
+++ version2/test.js
@@ -0,0 +1,81 @@
+// VERSION 2
+
+/*
+1. 
+a) Implement the following JavaScript library: https://www.chartjs.org/
+- Get a Bar chart working
+- You may use the example data from the docs
+
+b) Explain in 100 words or less your approach for implementing the library
+*/
+
+/*
+2.
+Using JavaScript only (adding HTML to index.html is NOT allowed), create a function that:
+- Takes as an argument an array of objects
+- Outputs the details of both books into the DOM 
+- Creates an unordered list for each book
+
+Use the following array of objects:
+*/
+const books = [
+  {
+    bookName: 'The Nature of Software Development',
+    author: 'Ron Jeffries',
+    coverURL:
+      'https://cdn-images-1.medium.com/max/1200/1*CQRh-pFTZ97ReXogbefleQ.png',
+  },
+  {
+    bookName: 'Clean Code',
+    author: 'Robert Cecil Martin',
+    coverURL:
+      'https://images-na.ssl-images-amazon.com/images/I/515iEcDr1GL._SX258_BO1,204,203,200_.jpg',
+  },
+  {
+    bookName: 'Refactoring',
+    author: 'Kent Beck & Martin Fowler',
+    coverUrl:
+      'https://s.s-bol.com/imgbase0/imagebase3/large/FC/3/0/2/0/9200000005430203.jpg',
+  },
+];
+
+/* 
+3. 
+Answer the following questions:
+- What is an Application Programming Interface (API)? 
+- How does this relate to your HackYourRepo project?
+
+Explain each in 200 words or less. 
+*/
+
+/*
+4.
+Write a function that:
+- Makes an API call using the Fetch API
+- Uses the following URL: https://jsonplaceholder.typicode.com/users
+- Make use of async/await syntax
+- Displays the "name", "email" and "city" of the first person inside an unordered list
+- Makes use of async/await
+*/
+
+/*
+5.
+a) Create a class, called Animal, that includes:
+- A constructor method
+- "type", "color", "size" properties
+- 1 additional method, called "getType", which returns the type
+
+b) Instantiate the class, and give it the name and attributes of a random animal
+
+c) Explain how this class relates Object-Oriented Programming (OOP) in 100 words or less
+*/
+
+/*
+6.
+Write a JavaScript function that:
+- Accepts a string of 4 words (all lowercase letters) as an argument
+- Converts the first letter of each word of the string in upper case.
+  
+  Example: 'the quick brown fox'
+  Expected Output: 'The Quick Brown Fox '
+*/
